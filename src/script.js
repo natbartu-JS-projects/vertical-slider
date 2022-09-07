a38a117372322f35ef975fa4df444321163d6874
natacha <nat_bartu@hotmail.com>
2022-09-07 20:07:11 +1000

    fix the order from the images for matching the sides of the application

--- src/script.js
+++ src/script.js
@@ -4,3 +4,10 @@ const slideLeft = document.querySelector(".left-slide");
 const upButton = document.querySelector(".up-button");
 const downButton = document.querySelector(".down-button");
 const slidesLength = slideRight.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upButton.addEventListener("click", () => changeSlide("up"));
+downButton.addEventListener("click", () => changeSlide("down"));
