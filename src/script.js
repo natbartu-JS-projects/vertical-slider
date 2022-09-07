4b336234f46219ed1dad5a5794407768b1dabbda
natacha <nat_bartu@hotmail.com>
2022-09-07 20:16:55 +1000

    add functionality into the right and left slides

--- src/script.js
+++ src/script.js
@@ -11,3 +11,20 @@ slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
 upButton.addEventListener("click", () => changeSlide("up"));
 downButton.addEventListener("click", () => changeSlide("down"));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
